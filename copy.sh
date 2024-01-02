f5ff04fe6da802d028c3635dc611dd2d23ad7968
contactayush <131809320+contactayush@users.noreply.github.com>
2024-01-02 13:23:54 +0530

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws configure set aws_access_key_id "" --profile user2 && aws configure set aws_secret_access_key "" && aws configure set region "ap-south-1" && aws configure set output "text"
+aws configure set aws_access_key_id "$AWS_SECRET_ACCESS_KEY" --profile user2 && aws configure set aws_secret_access_key "$AWS_ACCESS_KEY" && aws configure set region "ap-south-1" && aws configure set output "text"
