e2f75b57a19bb47d40c11b4374adffa4282514fa
contactayush <131809320+contactayush@users.noreply.github.com>
2024-01-02 13:33:58 +0530

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 aws configure set aws_access_key_id "$AWS_SECRET_ACCESS_KEY" --profile user2 && aws configure set aws_secret_access_key "$AWS_ACCESS_KEY" && aws configure set region "ap-south-1" && aws configure set output "text"
+aws s3 cp s3://buck-312-vel/java-project/gameoflife-web/target/gameoflife.war /mnt/servers/apache-tomcat-9.0.84/webapps
